ebaa13e73f3d0c3aaa6fad64d40e9a437754ba22
Luiz <ukangribner@hotmail.com>
2024-04-29 18:42:30 -0300

    Lista com alterações feita

---
+++ listaProdutos.js
@@ -0,0 +1,15 @@
+export default class ListaProdutos {
+
+    static abrirPaginaHome(){
+        console.log("Abrir home")
+    }
+    
+    static listarProdutos(){
+        console.log("Lista produtos")
+    }
+    
+    static getProduto(id){
+        console.log("Retorna detalhes")
+    }
+
+}
